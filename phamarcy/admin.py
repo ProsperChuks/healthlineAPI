9d91e99021517e028180427ef18a1a7eefc12305
ProsperChuks <prosperc40@gmail.com>
2023-09-07 00:45:02 +0100

    UserDB

--- phamarcy/admin.py
+++ phamarcy/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+
+class UserCreationFormExtended(UserCreationForm): 
+    class Meta:
+        model = User
+        fields = ['email', 'name']
+
+UserAdmin.add_form = UserCreationFormExtended
+UserAdmin.add_fieldsets = (
+    (None, {
+        'fields': ('name', 'email', 'username', 'password1', 'password2',)
+    }),
+)
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
--- phamarcy/models.py
+++ phamarcy/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=200, verbose_name='Name', help_text='Full Name')
+    email = models.EmailField(unique=True, max_length=50,  blank=False, null=False, verbose_name='Email')
+
+    def __str__(self) -> str:
+        return self.name
